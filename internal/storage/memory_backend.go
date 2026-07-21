8d8d306749fd06957edb9a273447b01e1bd8bc84
Rehaan1 <rehaanmazid@gmail.com>
2026-07-21 17:40:20 +0530

    feat: add get, put, delete

--- internal/storage/memory_backend.go
+++ internal/storage/memory_backend.go
@@ -1,11 +1,15 @@
 package storage
 
-import "sync"
+import (
+	"bytes"
+	"io"
+	"sync"
+)
 
 // MemoryBackend is an in-memory implementation of
 // Storage Backend.
 type MemoryBackend struct {
-	my   sync.RWMutex
+	mu   sync.RWMutex
 	data map[string][]byte
 }
 
@@ -15,4 +19,56 @@ func NewMemoryBackend() *MemoryBackend {
 	return &MemoryBackend{data: make(map[string][]byte)}
 }
 
-// TODO@mazidrehaan: Implement the StorageBackend Interface
+// Put is the Implementation of the StorageBackend Interface
+// Put function.
+func (m *MemoryBackend) Put(key string, data io.Reader) error {
+	// NOTE@mazidrehaan: We do not take lock here as
+	// reading data (aka upload) might take time and we
+	// dont want to block other readers meanwhile.
+	buf, err := io.ReadAll(data)
+
+	if err != nil {
+		return err
+	}
+
+	// Take write lock
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.data[key] = buf
+
+	return nil
+}
+
+// Get is the Implementation of the StorageBackend Interface
+// Get function.
+func (m *MemoryBackend) Get(key string) (io.ReadCloser, error) {
+
+	// Take read lock
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	buf, ok := m.data[key]
+
+	if !ok {
+		return nil, ErrNotFound
+	}
+
+	return io.NopCloser(bytes.NewReader(buf)), nil
+}
+
+// Delete is the Implementation of the StorageBackend Interface
+// Delete function.
+func (m *MemoryBackend) Delete(key string) error {
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	delete(m.data, key)
+
+	return nil
+}
+
+func (m *MemoryBackend) List(prefix string) ([]string, error) {
+	// @TODO
+	return nil, nil
+}
