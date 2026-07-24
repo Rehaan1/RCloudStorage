be6fe0d92510a7239553775b1e5a715269e67af5
Rehaan1 <rehaanmazid@gmail.com>
2026-07-24 20:13:26 +0530

    feat: add the get api

---
+++ internal/api/handlers.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"errors"
+	"net/http"
+	"rcloudstorage/internal/service"
+	"rcloudstorage/internal/storage"
+	"strconv"
+)
+
+func handleGet(svc *service.Service) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.PathValue("key")
+
+		data, meta, err := svc.Get(key)
+
+		switch {
+		case errors.Is(err, storage.ErrNotFound):
+			http.Error(w, "not found", http.StatusNotFound)
+			return
+
+		case err != nil:
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", meta.ContentType)
+		w.Header().Set("Content-Length", strconv.FormatInt(meta.Size, 10))
+		w.Write(data)
+	}
+}
